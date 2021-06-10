a6496eee734cd5abb5a9716d1a2ab47ba7972d0e
Maxim <xbultazar@gmail.com>
2021-06-10 21:09:05 +0300

    add menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,49 @@
+// адаптация выпадающего меню для мобильных устройств
+
+let isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+let body = document.querySelector('body');
+
+if (isMobile.any()) {
+    body.classList.add('touch');
+    let arrow = document.querySelectorAll('.arrow');
+    for (i = 0; i < arrow.length; i++) {
+
+        let thisLink = arrow[i].previousElementSibling;
+        let subMenu = arrow[i].nextElementSibling;
+        let thisArrow = arrow[i];
+
+        thisLink.classList.add('parentLinkMenu');
+        arrow[i].addEventListener('click', function () {
+            subMenu.classList.toggle('open');
+            thisArrow.classList.toggle('active');
+        });
+    }
+} else {
+    body.classList.add('mouse');
+}
+
+// воспроизведение главного видео на главной странице
+
 let animItems = document.querySelectorAll('._anim-items');
 
 if (animItems.length > 0) {
@@ -28,12 +74,12 @@ if (animItems.length > 0) {
         const rect = el.getBoundingClientRect(),
             scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
             scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return {top: rect.top + scrollTop, left: rect.left + screenLeft}
+        return { top: rect.top + scrollTop, left: rect.left + screenLeft }
     }
 
     setTimeout(() => {
         animOnScroll();
     }, 300)
 
-    
+
 }
